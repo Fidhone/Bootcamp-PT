a9b3ea5c3e85a0d156d12bee68642e3f95aedfa8
fidhone <fidhone@gmail.com>
2023-03-08 19:56:10 +0100

    hub

--- HUB/main.js
+++ HUB/main.js
@@ -1,10 +1,8 @@
-
 import { printTemplate as printLoginTemplate } from "./pages/Login/Login";
 import { printTemplate as printHomeTemplate } from "./pages/Home/Home";
 import { printTemplate as printPokedexTemplate } from "./pages/pokedex/Pokedex";
 
 import { addHeaderListeners } from "./utils/utils";
-
 
 
 export const initContent = (navigation) => {
